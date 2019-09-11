44cbb2c92c4102d4f9cad9be350aed34117b84c5
Aarts <geert.aarts@wur.nl>
2019-09-11 16:36:31 +0200

    Added xy project function

---
+++ R/xy.project.na.R
@@ -0,0 +1,14 @@
+xy.project.NA<-function(dataframe,CRSold,CRSnew,xold,yold,xnew,ynew)
+{
+  require(sp)
+  xy<-data.frame(dataframe[,xold],dataframe[,yold])
+  No.NAs<-which(!rowSums(is.na(xy))>0)
+  xy<-xy[No.NAs,]
+  coordinates(xy)<-names(xy)
+  proj4string(xy) <- CRS(CRSold)
+  xy<-as.data.frame(spTransform(xy, CRS(CRSnew)))
+   dataframe[No.NAs,xnew]<-xy[,1]
+  dataframe[No.NAs,ynew]<-xy[,2]
+  return(dataframe)
+}
+
