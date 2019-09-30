fcfb952de857e4235ea7bfc95947da0ade01f451
rvanbemmelen <rvanbemmelen@gmail.com>
2019-09-30 11:40:40 +0200

    added data loading and included option to zoom to Waddensea

--- R/utmmap.r
+++ R/utmmap.r
@@ -1,6 +1,13 @@
 
 # requires coast and ncp.utm shapefiles
 utmmap <- function(study.area=NULL, incl.ncp=FALSE, ...) {
+  if ( !exists('coast') ) {
+    load('data/NL_coast.rdata')
+  }
+  if ( !exists('ncp.utm') & incl.ncp==TRUE) {
+    load('data/NCP.rdata')
+  }
+  
   if (study.area=='VD' | is.null(study.area)) { # Voordelta
     xlim=c(435282.8, 628135.7)
     ylim=c(5650715, 5905695)
@@ -10,12 +17,16 @@ utmmap <- function(study.area=NULL, incl.ncp=FALSE, ...) {
     ylim <- c(5689000, 5930000)
   }
   if (study.area=='NS'){ # North Sea
-    xlim <- c(514000, 680000)
+    xlim <- c(0, 680000)
     ylim <- c(5689000,  6000000)
   }
   if (study.area=='NCP'){ # Dutch Continenal Shelf
     xlim <- c(464167,  744405.2) ## change!
     ylim <- c(5691582,  6179910) ## change!
+  }
+  if (study.area=='WS') { # Waddensea
+    xlim <- c(608000, 793000)
+    ylim <- c(5850000, 5938000 )
   }
   
   par(mar=rep(1,4))
