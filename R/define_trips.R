905218ae07a142343efaafb4dc2cff500d03ad25
Rob van Bemmelen <robvb@Rob-van-Bemmelen-MacBook-Pro.local>
2020-09-21 16:04:05 +0200

    some simple functions to define trips from the colony

---
+++ R/define_trips.R
@@ -0,0 +1,72 @@
+
+# simple functions to define trips
+
+# function: get time interval between subsequent fixes
+calc.d.time <- function(dt) { 
+  ndt <- length(dt)
+  if ( ndt ==1) {
+    out <- NA
+  } else {out <- c(abs(as.vector(difftime(time1=dt[1:(ndt-1)],
+                                          time2=dt[2:ndt], 
+                                          units='mins'))), NA) 
+  }
+  return(out)
+}
+# function: get distance between subsequent fixes
+calc.d.dist <- function(lat, lon) {
+  ndt <- length(lat)
+  if ( ndt==1 ) {
+    out <- NA
+  } else {
+    out <- c(argosfilter::distanceTrack(lat=lat, lon=lon), NA)
+  }
+  return(out)
+}
+# function: colony departure
+col.dep <- function(in_range) {
+  c(paste0(as.numeric(in_range[1:(length(in_range)-1)]), as.numeric(in_range[2:(length(in_range))]))=="10", NA)
+}
+col.arr <- function(in_range) {
+  c(NA, paste0(as.numeric(in_range[1:(length(in_range)-1)]), as.numeric(in_range[2:(length(in_range))]))=="01")
+}
+# function: which time gaps are larger than a threshold value?
+dt.gap <- function(date_time, max.gap) { # max gap in minutes
+  dt <- calc.d.time(date_time)
+  dt <- c(NA, dt[-length(dt)])
+  t.gap <- dt > max.gap
+  return(t.gap)
+}
+# function: create trip index: split track between colony departures
+trips.index <- function(dep, gap) {
+  ri <- 1:length(dep)
+  gap[1] <- FALSE
+  br <- c(which(dep | gap)-1, length(dep))
+  if ( br[1]!=1 ) {
+    br <- c(1, br)
+  }
+  ct <- cut(ri, breaks=br, include.lowest = TRUE, labels=1:(length(br)-1))
+  return(ct)
+}
+# function: time since colony departure
+time.since.departure <- function(time, inrange) {
+  coldep <- col.dep(inrange) # colony departure
+  if (first(coldep)==1) {
+    out <- as.vector(difftime(
+      time1=time,
+      time2=time[1],
+      units="mins"))
+    out[inrange==1] <- 0
+  } else {
+    out <- rep(NA, length(time))
+  }
+  return(out)
+}
+completeness <- function(inrange) {
+  cd <- any(col.dep(inrange), na.rm=TRUE) # departure
+  ca <- any(col.arr(inrange), na.rm=TRUE) # arrival
+  if (ca & cd) { out <- "both" }
+  if (ca & !cd) { out <- "start" }
+  if (!ca & cd) { out <- "end" }
+  if (!ca & !cd) { out <- "none" }
+  return(out)
+}
